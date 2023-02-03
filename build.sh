85e3d0e77752aa41a85ed9d53dff2c67bb54ca63
wahyudindra <wahyudi.indra@glovory.com>
2023-02-03 14:19:16 +0700

    Build resume

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 python resume.py --chrome-path="C:\Program Files\Google\Chrome\Application\chrome.exe"
-mv "C:\Program Files\Google\Chrome\Application\109.0.5414.75\resume.pdf" .
+mv "C:\Program Files\Google\Chrome\Application\109.0.5414.120\resume.pdf" .
